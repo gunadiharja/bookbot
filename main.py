e74c544765733e0df0cac03025897f59fbb8a770
gunadiharja <agunadiharja@gmail.com>
2025-10-03 20:58:50 +0700

    add main functionalities

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+from stats import count_words, sort_count
+
 def get_book_path(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
-    
-def count_words(text):
-    text_to_count = text.split()
-    num_words = len(text_to_count)
-    return f"Found {num_words} total words"
 
 def main():
     text = get_book_path("books/frankenstein.txt")
-    print(count_words(text))
+    count = count_words(text)
+    char_count = sort_count(text)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(count)
+    print("--------- Character Count -------")
+    for i in char_count:
+        if i['char'].isalpha():
+            print(f"{i["char"]}: {i["num"]}")
+    print("============= END ===============")
+        
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,19 @@ def count_char(text):
             char[i] = 0
         char[i] += 1 
     return(char)
+
+# helper function
+def sort_on(text):
+    return text["num"]
+
+def sort_count(text):
+    data = count_char(text)
+    list_dict = []
+    for k, v in data.items():
+        data_dict = {}
+        data_dict["char"] = k
+        data_dict["num"] = v
+        list_dict.append(data_dict)
+
+    list_dict.sort(reverse=True, key=sort_on)   
+    return list_dict
